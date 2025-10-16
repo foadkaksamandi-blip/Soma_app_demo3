fae290fae1f8fa3b77ff64e6c24c0ca9f3e86657
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-16 14:39:51 +0330

    Update buyer_main.dart

--- lib/buyer_main.dart
+++ lib/buyer_main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
-import 'buyer_home_page.dart';
+import 'screens/buyer_home_page.dart';
 
-void main() {
-  runApp(const BuyerApp());
-}
+void main() => runApp(const BuyerApp());
 
 class BuyerApp extends StatelessWidget {
   const BuyerApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'SOMA Buyer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        fontFamily: 'Vazirmatn', // اگر فونت فارسی داری
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
-        ),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        appBarTheme: const AppBarTheme(foregroundColor: Colors.white, backgroundColor: Colors.blue),
       ),
       home: const BuyerHomePage(),
       localizationsDelegates: const [
@@ -28,10 +21,7 @@ class BuyerApp extends StatelessWidget {
         DefaultWidgetsLocalizations.delegate,
         DefaultCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('fa', 'IR'),
-        Locale('en', 'US'),
-      ],
+      supportedLocales: const [Locale('fa', 'IR'), Locale('en', 'US')],
     );
   }
 }
